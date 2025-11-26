e5bd5f36a209f3c53cbb398f3ce109c5111a51b9
DavidKal29 <davidkalmuk65@gmail.com>
2025-11-26 12:21:53 +0100

    Modificamos la ruta de getRandomAnimals para que devuelva unicamente el id, nombre y tipo, puesto que solo se mostrará eso de los animales

--- animals/serializer.py
+++ animals/serializer.py
@@ -1,8 +1,18 @@
-from .models import Animals
+from .models import Animals, AnimalTypes, Types
 from rest_framework.serializers import ModelSerializer
 
 
 class AnimalsSerializer(ModelSerializer):
     class Meta:
         model = Animals
+        fields = '__all__'
+
+class TypesSerializer(ModelSerializer):
+    class Meta:
+        model = Types
+        fields = '__all__'
+
+class AnimalTypesSerializer(ModelSerializer):
+    class Meta:
+        model = AnimalTypes
         fields = '__all__'
--- animals/views.py
+++ animals/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Animals
 from .serializer import AnimalsSerializer
+from django.db import connection
 
 # Create your views here.
 @api_view(['GET'])
@@ -14,6 +15,28 @@ def getAnimals(request):
 
 @api_view(['GET'])
 def getRandomAnimals(request):
-    random_animals = Animals.objects.order_by('?')[:12]
-    serializer = AnimalsSerializer(random_animals,many=True)
-    return Response({'success':'Animales obtenido con éxito','animals':serializer.data})  
+
+    with connection.cursor() as cursor:
+        query = '''
+            SELECT a.id, a.name, t.name 
+            FROM byyaf0imkignvliwwex9.animals as a
+            INNER JOIN byyaf0imkignvliwwex9.animal_types as at
+            ON a.id = at.id_animal
+            INNER JOIN byyaf0imkignvliwwex9.types as t
+            ON at.id_type = t.id
+            ORDER BY RAND()
+            LIMIT 12        
+        '''
+        cursor.execute(query)
+
+        rows = cursor.fetchall()
+
+        animals = []
+        for row in rows:
+            animals.append({
+                'id':row[0],
+                'name':row[1],
+                'type':row[2]
+            })
+
+    return Response({'success':'Animales obtenido con éxito','animals':animals})  
